d4ce1a09ec1a3fdac53e32743c5677ed7db1db6a
biafraga <beatrizcfraga3@gmail.com>
2026-05-06 10:22:16 -0300

    Alterações do dia

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 //Arquivo que chama a tela principal
 // O App chama a tela principal
 
-import Home from "./components/Home";
+import Home from "./screens/Home";
 
 export default function App(){
     return <Home />
